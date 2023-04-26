e4fa29014e4111695f0333508c5c3f924fff5722
www.luyifeng333@sina.cn <www.luyifeng333@sina.cn>
2023-04-26 19:47:08 +0800

    java

---
+++ setchange.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class setchange {
+
+
+    public static void main(String[] args) {
+        int a = 233;
+        byte b = (byte) a;
+
+        System.out.println(a + b);
+        float c=3.98f;
+        int d=(int)c;
+        System.out.println(c);
+        String e="23";
+        int f=Integer.parseInt(e);
+        System.out.println(f);
+        char g=(char)f ;
+        System.out.println(f);
+        int [] h;
+         h=new int[] {1,2,3,4,5};
+        System.out.println(Arrays.toString(h));
+        boolean w=true;double m=3.14;short p=20000;
+        System.out.println(p);
+    }
+}
